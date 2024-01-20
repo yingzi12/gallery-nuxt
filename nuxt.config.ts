4388b7e40f10571b35a7466dc16e306398cc9778
卢煌 <luhuang@jtexpress.com>
2024-01-20 15:26:03 +0800

    解决图片显示的问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,8 +22,8 @@ export default defineNuxtConfig({
     },
     // Public keys that are exposed to the client
     public: {
-      // baseUrl:  "https://test.aiavr.uk",
-      baseUrl:  "http://127.0.0.1:8098",
+      baseUrl:  "https://test.aiavr.uk",
+      // baseUrl:  "http://127.0.0.1:8098",
       sourceWeb: 'https://image.51x.uk/xinshijie/test'
     }
   },
